b1bd15880d25850946f888362d1d8253978af767
Fabio Milheiro <fabiomilheiro@gmail.com>
2020-04-19 10:38:56 +0100

    Tested unresolved bugs selector.

--- redux-starter/src/store/tests/bugs.spec.js
+++ redux-starter/src/store/tests/bugs.spec.js
@@ -19,6 +19,20 @@ describe("Bugs", () => {
       bugsOnServer.push(newBug);
       return [200, newBug];
     });
+    axiosMock.onPatch(/\/bugs\/\d+/).reply((config) => {
+      const bug = bugsOnServer.find(
+        (b) => b.id == /\/(?<id>\d+)/.exec(config.url).groups.id
+      );
+      const patchData = JSON.parse(config.data);
+
+      if (patchData.resolved !== undefined) {
+        bug.resolved = patchData.resolved;
+      }
+
+      bug.userId = patchData.userId || bug.userId;
+
+      return [200, bug];
+    });
   });
 
   describe("Load bugs", () => {
@@ -58,15 +72,6 @@ describe("Bugs", () => {
     });
 
     it("assigns user to the bug", async () => {
-      axiosMock.onPatch(/\/bugs\/\d+/).reply((config) => {
-        const bug = bugsOnServer.find(
-          (b) => b.id == /\/(?<id>\d+)/.exec(config.url).groups.id
-        );
-        const patchData = JSON.parse(config.data);
-        bug.userId = patchData.userId || bug.userId;
-
-        return [200, bug];
-      });
       const bug = getBugs().list[0];
 
       await store.dispatch(bugs.actions.assign(bug.id, 555));
@@ -83,18 +88,6 @@ describe("Bugs", () => {
 
     it("sets the bug as resolved if patched", async () => {
       const bug = getBugs().list[0];
-      axiosMock.onPatch(/\/bugs\/\d+/).reply((config) => {
-        const bug = bugsOnServer.find(
-          (b) => b.id == /\/(?<id>\d+)/.exec(config.url).groups.id
-        );
-        const patchData = JSON.parse(config.data);
-
-        if (patchData.resolved !== undefined) {
-          bug.resolved = patchData.resolved;
-        }
-
-        return [200, bug];
-      });
 
       await store.dispatch(bugs.actions.resolve(bug.id));
 
@@ -111,6 +104,30 @@ describe("Bugs", () => {
       const updated = getBug(bug.id);
       expect(updated.resolved).toBeFalsy();
     });
+  });
+
+  describe("selectors", () => {
+    it("Get unresolved bugs", async () => {
+      await store.dispatch(bugs.actions.add("A"));
+      await store.dispatch(bugs.actions.add("B"));
+      await store.dispatch(bugs.actions.add("C"));
+      const bugA = getBugByDescription(store, "A");
+      const bugB = getBugByDescription(store, "B");
+      const bugC = getBugByDescription(store, "C");
+      await store.dispatch(bugs.actions.resolve(bugA.id));
+
+      const result = bugs.selectors.getUnresolvedBugs(store.getState());
+
+      expect(result).not.toContain(bugA);
+      expect(result).toContain(bugB);
+      expect(result).toContain(bugC);
+    });
+
+    function getBugByDescription(store, description) {
+      return store
+        .getState()
+        .entities.bugs.list.find((b) => b.description === description);
+    }
   });
 
   function getBug(id) {
